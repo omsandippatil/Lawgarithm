925dc7a779b009b179f6edb6554850148ef02c43
Om  Patil <omsandeeppatil02@gmail.com>
2025-06-23 15:11:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-      GROQ_API_KEY: "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",
+      GROQ_API_KEY: "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",
 };
